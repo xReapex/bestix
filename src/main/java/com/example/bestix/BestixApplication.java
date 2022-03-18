17d897b8654e737da4a38bd4df2562b96c2ac74c
ESK <ert.fb6@gmail.com>
2022-03-18 10:21:13 +0100

    initial commit

---
+++ src/main/java/com/example/bestix/BestixApplication.java
@@ -0,0 +1,13 @@
+package com.example.bestix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bestix/BestixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bestix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
