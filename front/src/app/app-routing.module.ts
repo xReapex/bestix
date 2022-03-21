188bc8a7bc6c6e67bde441cdf3a238a2899b5b20
xReapex <tr7296228@gmail.com>
2022-03-21 14:30:09 +0100

    Adding front login form

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { LoginComponent } from './login/login.component';
+
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot([
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+    ]),
+  ],
+
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
