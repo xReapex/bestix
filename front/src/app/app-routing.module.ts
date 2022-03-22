1187892d7d008ea7fb7c595fa00410ed4b2f3873
xReapex <tr7296228@gmail.com>
2022-03-22 18:20:12 +0100

    Update front

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
+import { LandingComponent } from './landing/landing.component';
 
 const routes: Routes = [];
 
@@ -12,6 +13,10 @@ const routes: Routes = [];
         path: 'login',
         component: LoginComponent,
       },
+      {
+        path: '**',
+        component: LandingComponent
+      }
     ]),
   ],
 
