016a66cf92f19012d2afc0d16cce305d874d0621
xReapex <thomas1.robert1@gmail.com>
2022-03-24 12:04:19 +0100

    Dashboard init

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { LandingComponent } from './landing/landing.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BettingCardComponent } from './betting-card/betting-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    LandingComponent
+    LandingComponent,
+    DashboardComponent,
+    BettingCardComponent
   ],
   imports: [
     BrowserModule,
