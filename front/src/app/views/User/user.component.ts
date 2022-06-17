eb299fb8ab714d1f610a2048ced1505db696cd2d
kiman <annabelle.bosse>
2022-06-17 09:58:44 +0200

    OK

--- front/src/app/views/User/user.component.ts
+++ front/src/app/views/User/user.component.ts
@@ -1,19 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { HttpClient } from '@angular/common/http';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
 @Component({
-  selector: 'app-login',
+  selector: 'user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
 
-  constructor(private router: Router) { }
+//PUT - Change User name by Id
+currentPutUserId = '';
+currentPutUserPassword = '';
+
+  //PUT user 
+  putUserPasswordByIdInput = new FormControl();
+
+
+
+  constructor(private  httpClient: HttpClient) { }
+  public UserPassword: string | undefined;
+
 
   ngOnInit(): void {
   }
+  putUserPasswordById() {
+    let id = this.currentPutUserId;
+    let password = this.currentPutUserPassword;
+    let body = { password }
 
-  btnClick() {
-    this.router.navigateByUrl('/');
-  };
+    this.httpClient.put('/api/user/' + id, password).subscribe(response => {
+      console.log("[PUT] from back : ", response);
+    }, error => {
+      console.log("[PUT] Erreur", error);
+    })
+    console.log("[EVENT] Button PUT Player pressed");
+  }
+
 }
