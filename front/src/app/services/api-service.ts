c1615dcd7b844c61e2cdeff16dde6de28a74acab
xReapex <tr7296228@gmail.com>
2022-06-28 09:19:40 +0200

    try to fix

--- front/src/app/services/api-service.ts
+++ front/src/app/services/api-service.ts
@@ -23,59 +23,59 @@ export class ApiService {
 
   public getFavoritesByUserId(id: string)
   {
-    return this.http.get("https://45.147.98.151:8080/api/Favorites/userId=" + id);
+    return this.http.get("http://45.147.98.151:8080/api/Favorites/userId=" + id);
   }
 
   public getPlayerInfos(id: string)
   {
-    return this.http.get("https://45.147.98.151:8080/api/external/players/" + id);
+    return this.http.get("http://45.147.98.151:8080/api/external/players/" + id);
   }
 
   public savePlayerAsFavorite(id: string)
   {
     let defaultUserIdForTests = 0;
 
-    return this.http.post("https://45.147.98.151:8080/api/Favorites/playerId=" + id + "&userId=" + defaultUserIdForTests, null);
+    return this.http.post("http://45.147.98.151:8080/api/Favorites/playerId=" + id + "&userId=" + defaultUserIdForTests, null);
   }
 
   public deleteFavorite(id: string)
   {
     let defaultUserIdForTests = 0;
-    return this.http.delete("https://45.147.98.151:8080/api/Favorites/playerId=" + id + "&userId=" + defaultUserIdForTests);
+    return this.http.delete("http://45.147.98.151:8080/api/Favorites/playerId=" + id + "&userId=" + defaultUserIdForTests);
   }
 
   public getLeaguesList()
   {
-    return this.http.get("https://45.147.98.151:8080/api/league/list");
+    return this.http.get("http://45.147.98.151:8080/api/league/list");
   }
 
   public requestLeagueAccess(leagueId: string, password: string, userId: string)
   {
-    return this.http.post("https://45.147.98.151:8080/api/league/" + leagueId + "/" + password + "/" + userId, null);
+    return this.http.post("http://45.147.98.151:8080/api/league/" + leagueId + "/" + password + "/" + userId, null);
   }
 
   public checkJoinedLeaguesByUserId(userId: string)
    {
-     return this.http.get("https://45.147.98.151:8080/api/league/" + userId);
+     return this.http.get("http://45.147.98.151:8080/api/league/" + userId);
    }
 
   public getLeagueUserList(leagueId: string)
   {
-    return this.http.get("https://45.147.98.151:8080/api/league/" + leagueId + "/users");
+    return this.http.get("http://45.147.98.151:8080/api/league/" + leagueId + "/users");
   }
 
   public getMatchListFromBack()
   {
-    return this.http.get("https://45.147.98.151:8080/api/matchs/scheduled");
+    return this.http.get("http://45.147.98.151:8080/api/matchs/scheduled");
   }
 
   public saveMatchBetByUserId(matchId: string, teamId: string, userId: string)
   {
-    return this.http.post("https://45.147.98.151:8080/api/bet/matchId=" + matchId + "&teamId=" + teamId + "&userId=" + userId, null);
+    return this.http.post("http://45.147.98.151:8080/api/bet/matchId=" + matchId + "&teamId=" + teamId + "&userId=" + userId, null);
   }
 
   public getBetResultsByUserId(userId: string)
   {
-    return this.http.get("https://45.147.98.151:8080/api/bet/userId=" + userId + "/results");
+    return this.http.get("http://45.147.98.151:8080/api/bet/userId=" + userId + "/results");
   }
 }
